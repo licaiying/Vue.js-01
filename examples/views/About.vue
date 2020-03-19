db3c8afa37fd8c0563bae536c01bb31d294ff64f
licaiying <1194449590@qq.com>
2020-03-19 10:57:22 +0800

    给按钮设置icon图标属性

--- examples/views/About.vue
+++ examples/views/About.vue
@@ -3,16 +3,16 @@
     <my-headline></my-headline>
     <my-button></my-button>
     <br />
-    <my-button size="large">我是自定义的内容1</my-button>
+    <my-button size="large" icon="tty">我是自定义的内容1</my-button>
     <br />
-    <my-button size="small">我是自定义的内容2</my-button>
+    <my-button size="small" icon="deaf">我是自定义的内容2</my-button>
     <br />
-    <my-button size="massive">我是自定义的内容1</my-button>
+    <my-button size="massive" icon="sign language">我是自定义的内容1</my-button>
     <br />
-    <my-button size="small">我是自定义的内容2</my-button>
+    <my-button size="small" icon="volume up">我是自定义的内容2</my-button>
     <br />
-    <my-button size="large">我是自定义的内容1</my-button>
+    <my-button size="large" icon="video">我是自定义的内容1</my-button>
     <br />
-    <my-button size="massive">我是自定义的内容2</my-button>
+    <my-button size="massive" icon="coffee">我是自定义的内容2</my-button>
   </div>
 </template>
--- packages/button/button.vue
+++ packages/button/button.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <button class="ui button" :class="size">
+      <i v-if="icon" class="icon" :class="icon"></i>
       <slot>我是默认的内容</slot>
     </button>
   </div>
@@ -17,8 +18,21 @@ export default {
         // console.log(val) // 'mini', 'tiny', 'small', 'medium'.....
         // 只要传入 size属性，就会进入到这个函数中，如返回true,则表示生效,如返回false，则表示不允许
         // 如果传入的size是：mini,medium,huge,massive.... 就ok,否则就返回false
-        return ['mini', 'tiny', 'small', 'medium', 'large', 'big', 'huge', 'massive'].includes(val)
+        return [
+          'mini',
+          'tiny',
+          'small',
+          'medium',
+          'large',
+          'big',
+          'huge',
+          'massive'
+        ].includes(val)
       }
+    },
+    icon: {
+      type: String,
+      required: false
     }
   },
   data () {
